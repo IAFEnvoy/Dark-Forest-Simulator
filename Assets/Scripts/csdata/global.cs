8475f1875a573442f94b85423438247395fef2b6
IAFEnvoy <tanksheng545@126.com>
2021-07-21 23:04:51 +0800

    加入从外部读取启动参数的模块

--- Assets/Scripts/csdata/global.cs
+++ Assets/Scripts/csdata/global.cs
@@ -6,7 +6,7 @@
     public static int develop = 10;//发展速度
     public static int cooperation = 5;//合作的时候额外的发展速度
     public static int attack = -30;//战争的时候额外的发展速度
-                                   //（通常设置为小于-10的数，当然你也可以设置为正的。。。）
+                                   //（通常设置为小于-develop的数，当然你也可以设置为正的。。。）
     public static bool allowspawn = true;//是否生成新文明
     public static int spawnprobability = 100;//生成概率，填1000则每一次update有1/1000的概率生成新文明
     public static int cooldowntime = 500;//需要多少score才能发射飞船
---
+++ Assets/Scripts/init.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(File.Exists(Application.dataPath + @"\start_up_parameter.txt")){
+            StreamReader sr=new StreamReader(Application.dataPath + @"\start_up_start_up_parameter.txt");
+            global.startcnt=int.Parse(sr.ReadLine());
+            global.startscore=int.Parse(sr.ReadLine());
+            global.travel_speed=double.Parse(sr.ReadLine());
+            global.develop=int.Parse(sr.ReadLine());
+            global.cooperation=int.Parse(sr.ReadLine());
+            global.attack=int.Parse(sr.ReadLine());
+            global.allowspawn=bool.Parse(sr.ReadLine());
+            global.spawnprobability=int.Parse(sr.ReadLine());
+            global.cooldowntime=int.Parse(sr.ReadLine());
+            global.rangex=double.Parse(sr.ReadLine());
+            global.rangey=double.Parse(sr.ReadLine());
+            global.rangez=double.Parse(sr.ReadLine());
+            global.defensetimes=int.Parse(sr.ReadLine());
+            global.peace=int.Parse(sr.ReadLine());
+            global.middle=int.Parse(sr.ReadLine());
+            global.attacks=int.Parse(sr.ReadLine());
+            sr.Close();
+        }
+    }
+}
