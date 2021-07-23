1077fd6ac699c7220ac11cd2fdefbc107aa43136
IAFEnvoy <tanksheng545@126.com>
2021-07-23 16:55:56 +0800

    加入快速关闭

--- Assets/Scripts/ButtonMethods/Exit.cs
+++ Assets/Scripts/ButtonMethods/Exit.cs
@@ -13,4 +13,12 @@ public class Exit : MonoBehaviour
         if (KeyBoardControl.show)
             UnityEngine.Application.Quit();
     }
+    void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+        Debug.Log("当前进程名：" + System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+        if(System.Diagnostics.Process.GetCurrentProcess().ProcessName.Contains("Unity"))
+            Debug.Log("检测到为编辑器环境，将不执行快速关闭代码");
+        else System.Diagnostics.Process.GetCurrentProcess().Kill();
+    }
 }
