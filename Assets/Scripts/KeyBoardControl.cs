c23a20b4ba9ec612f78f64f6275784270b1d74f0
IAFEnvoy <tanksheng545@126.com>
2021-07-24 23:40:37 +0800

    加入F2截图

--- Assets/Scripts/KeyBoardControl.cs
+++ Assets/Scripts/KeyBoardControl.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
 
 public class KeyBoardControl : MonoBehaviour
 {
@@ -43,6 +46,40 @@ public class KeyBoardControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
         {
             Calculate.sortscore = !Calculate.sortscore;
+        }
+        if (Input.GetKeyDown(KeyCode.F2))//截屏系统
+        {
+            Rect r=new Rect(0,0,Screen.width,Screen.height);
+            Camera came=GameObject.Find("Main Camera").GetComponent<Camera>();
+            RenderTexture rt = new RenderTexture((int)r.width, (int)r.height, 0);
+
+            came.targetTexture = rt;
+            came.Render();
+            RenderTexture.active = rt;
+            Texture2D screenShot = new Texture2D((int)r.width, (int)r.height, TextureFormat.RGB24, false);
+            screenShot.ReadPixels(r, 0, 0);
+            screenShot.Apply();
+            came.targetTexture = null;
+            RenderTexture.active = null;
+            GameObject.Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+
+            OpenFileName openFileName = new OpenFileName();
+            openFileName.structSize = Marshal.SizeOf(openFileName);
+            openFileName.filter = "*.png\0*.png";
+            openFileName.file = new string(new char[256]);
+            openFileName.maxFile = openFileName.file.Length;
+            openFileName.fileTitle = new string(new char[64]);
+            openFileName.maxFileTitle = openFileName.fileTitle.Length;
+            openFileName.initialDir = (UnityEngine.Application.dataPath).Replace('/', '\\');//默认路径
+            openFileName.title = "保存截图";
+            openFileName.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;
+
+            if (LocalDialog.GetSaveFileName(openFileName))
+            {
+                if (openFileName.file.Contains(".png") == false) openFileName.file += ".png";
+                File.WriteAllBytes(openFileName.file,bytes);
+            }
         }
     }
 }
